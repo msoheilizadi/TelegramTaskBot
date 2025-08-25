2833c735e3bf6cd63ee5f3a82ad4bac3fb9d6e9c
msoheilizadi <your-email@example.com>
2025-08-25 14:32:50 +0330

    change to pool

--- storage/db/addTasksToDb.js
+++ storage/db/addTasksToDb.js
@@ -1,18 +1,4 @@
-const { Client } = require('pg'); 
-require('dotenv').config();
-
-const db = new Client({
-    user:  process.env.DB_USER,
-    host:  process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port:   process.env.DB_PORT,
-    ssl: { rejectUnauthorized: false }
-})
-
-db.connect()
-  .then(() => console.log("Connected to PostgreSQL!"))
-  .catch(err => console.error("Connection error", err.stack));
+const db = require('./connectToDb');
 
 async function addNewTask(userid, taskTitle, todayDate) {
     try {
